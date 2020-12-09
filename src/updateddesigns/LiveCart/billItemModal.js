39476fe71596a892f339b87f7a2520583b4829a3
kalyani <kalyani.kalyanam@possibilliontech.com>
2020-12-09 12:01:51 +0530

    billitem modal changes

--- src/updateddesigns/LiveCart/billItemModal.js
+++ src/updateddesigns/LiveCart/billItemModal.js
@@ -13,6 +13,14 @@ const BillItemModal = ({ item }) => {
       </td>
       <td style={{ textAlign: "right", padding: "3px 10px", fontSize: "35px" }}>
         <b>{item && item.quantity * item.price}</b>
+
+        {item.discount > 0 ? (
+          <b>
+            {`(Discount ₹ ${parseFloat(
+              ((item.price * item.discount) / 100) * item.quantity
+            ).toFixed(2)} )`}
+          </b>
+        ) : null}
       </td>
     </tr>
   );
