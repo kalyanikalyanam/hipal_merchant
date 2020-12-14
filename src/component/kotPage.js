393d86ab97fa1638c32fd9c7d4ad8ef20d9b175a
kalyani <kalyani.kalyanam@possibilliontech.com>
2020-12-14 14:11:09 +0530

    digitalkot

--- src/component/kotPage.js
+++ src/component/kotPage.js
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { db } from '../config'
+import React, { useEffect, useState } from "react";
+import { db } from "../config";
 
 const KotPage = () => {
-    const [itmes, setItems] = useState([])
-    useEffect(() => {
-        const businessId = sessionStorage.getItem("businessId")
-        var unsubscribe
-        const getItems = async () => {
-            const ref = db 
-                .collection("kotItems")
-                .where("businessId", "==", businessId)
-            const querySnapshot = await ref.get()
-            let items = []
-            querySnapshot.forEach(childSnapshot => items.push({...childSnapshot.data()}))
-            setItems(items)
+  const [itmes, setItems] = useState([]);
+  useEffect(() => {
+    const businessId = sessionStorage.getItem("businessId");
+    var unsubscribe;
+    const getItems = async () => {
+      const ref = db
+        .collection("kotItems")
+        .where("businessId", "==", businessId);
+      const querySnapshot = await ref.get();
+      let items = [];
+      querySnapshot.forEach((childSnapshot) =>
+        items.push({ ...childSnapshot.data() })
+      );
+      setItems(items);
 
-            unsubscribe = ref.onSnapshot(querySnapshot => {
-                let items = []
-                querySnapshot.forEach(childSnapshot => items.push({ ...childSnapshot.data() }))
-                setItems(items)
-            })
-        }
-        getItems()
-        return unsubscribe
-    }, [])
+      unsubscribe = ref.onSnapshot((querySnapshot) => {
+        let items = [];
+        querySnapshot.forEach((childSnapshot) =>
+          items.push({ ...childSnapshot.data() })
+        );
+        setItems(items);
+      });
+    };
+    getItems();
+    return unsubscribe;
+  }, []);
 
-    return (
-        <div>
-            {itmes && itmes.map(item => (
-                <div key={item.id}>
-                    {item.name}
-                </div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      {itmes && itmes.map((item) => <div key={item.id}>{item.name}</div>)}
+    </div>
+  );
+};
