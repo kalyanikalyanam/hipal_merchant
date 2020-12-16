31faaef83e9ec891d14b0fa18af607c99980f235
kalyani <kalyani.kalyanam@possibilliontech.com>
2020-12-16 15:33:41 +0530

    history changes

--- src/component/KotPage/cardView.js
+++ src/component/KotPage/cardView.js
@@ -1,190 +1,192 @@
-import React, { useEffect, useState } from 'react'
-import Timer from 'react-compound-timer'
-import { db } from '../../config'
+import React, { useEffect, useState } from "react";
+import Timer from "react-compound-timer";
+import { db } from "../../config";
 
+const CardView = ({ kots }) => {
+  const [kotItems, setKotItems] = useState([]);
+  useEffect(() => {
+    let kotItems = kots;
+    kotItems = kotItems.filter((kot) => kot.status !== "served");
+    setKotItems(kotItems);
+  }, [kots]);
 
-const CardView = ({kots}) => {
-    const [kotItems, setKotItems] = useState([])
-    useEffect(() => {
-        let kotItems = kots
-        kotItems = kotItems.filter(kot => kot.status !== 'served')
-        setKotItems(kotItems)
-    }, [kots])
+  const handleTimerStop = (kot) => {
+    const newKot = kot;
+    newKot.status = "served";
+    setTimeout(() => {
+      db.collection("kotItems").doc(kot.id).update(newKot);
+    }, [2000]);
+  };
+  const handleServed = async (kot) => {
+    let newKot = kot;
+    let newItems = newKot.items;
+    newItems.forEach((item) => {
+      item.status = "served";
+    });
+    const ref = db.collection("tables").doc(kot.tableId);
 
-    const handleTimerStop = (kot) => {
-        const newKot = kot
-        newKot.status = 'served'
-        setTimeout(() => {
-            db.collection('kotItems').doc(kot.id).update(newKot)
-        }, [2000])
-    }
-    const handleServed = async (kot) => {
-        let newKot = kot
-        let newItems = newKot.items
-        newItems.forEach(item => {
-            item.status = "served"
-        })
-        const ref =  db
-            .collection('tables')
-            .doc(kot.tableId)
+    await db.collection("kotItems").doc(kot.id).update(newKot);
 
-        await db
-            .collection('kotItems')
-            .doc(kot.id)
-            .update(newKot)
+    const table = await ref.get();
 
-        const table = await ref.get()
+    let orders = table.data().orders;
+    orders.forEach((item) => {
+      if (item.orderPageId === item.orderPageId) {
+        item.status = "served";
+      }
+    });
 
-        let orders = table.data().orders
-        orders.forEach(item => {
-            if(item.orderPageId === item.orderPageId){
-                item.status = 'served'
-            }
-        })
+    ref.update({
+      orders,
+    });
+  };
 
-        ref.update({
-            orders
-        })
-    }
-
-    const handleCheckMark = async (it , kot) => {
-        let flag = false
-        let newKot = kot
-        let newItems = newKot.items
-        if(it.status === 'served'){
-            flag = true
-            newItems.forEach(item => {
-                if (item.id === it.id) {
-                    item.status = "cooking"
-                }
-            })
+  const handleCheckMark = async (it, kot) => {
+    let flag = false;
+    let newKot = kot;
+    let newItems = newKot.items;
+    if (it.status === "served") {
+      flag = true;
+      newItems.forEach((item) => {
+        if (item.id === it.id) {
+          item.status = "cooking";
         }
-        else {
-            newItems.forEach(item => {
-                if (item.id === it.id) {
-                    item.status = "served"
-                }
-            })
+      });
+    } else {
+      newItems.forEach((item) => {
+        if (item.id === it.id) {
+          item.status = "served";
         }
-
-        await db
-            .collection('kotItems')
-            .doc(kot.id)
-            .update(newKot)
-
-        const ref =  db
-            .collection('tables')
-            .doc(kot.tableId)
-
-        const table = await ref.get()
-
-        let orders = table.data().orders
-        orders.forEach(item => {
-            if(item.orderPageId === it.orderPageId){
-                if(flag) item.status = "cooking"
-                else item.status = "served"
-            }
-        })
-        ref.update({
-            orders
-        })
+      });
     }
-    return (
-        <div className="list-kot">
-        {kotItems && kotItems.map(kot => {
-            let served = false
-            let ready = 0
-            kot.items.forEach(item => {
-                if (item.status === 'served') {
-                    ready++
-                }
-            })
-            if(ready === kot.items.length){
-                served = true
 
-            } 
-            return (
-                <div className="box-kot" key={kot.id}>
-                    <div className={served ? 'kot-card selected' : 'kot-card'}>
-                        <div className="headrow">
-                            <h1>
-                                {kot.type || "DineIn"}{" "}
-                                <i
-                                    className="fa fa-circle dinein_color"
-                                    aria-hidden="true"
-                                ></i>
-                                <span>
-                                    <i className="fas fa-ellipsis-v"></i>
-                                </span>
-                            </h1>
-                        </div>
+    await db.collection("kotItems").doc(kot.id).update(newKot);
 
-                        <div className="main-head">
-                            <span>Table: {kot.tableName}</span>
-                            <span>
-                                <Timer
-                                    initialTime={Date.now() - kot.createdOn}
-                                    onStop={() => handleTimerStop(kot)}
-                                >
-                                    {({ start, stop }) => {
-                                        if(served)stop()
-                                    return (
-                                        <React.Fragment>
-                                            <Timer.Hours />:
-                                            <Timer.Minutes />:
-                                            <Timer.Seconds />
-                                        </React.Fragment>
-                                        )
-                                    }}
+    const ref = db.collection("tables").doc(kot.tableId);
 
-                                </Timer>
-                            </span>
-                        </div>
+    const table = await ref.get();
 
-                        <div className="waiterrow">{kot.orderId || `0931280AASD90`}</div>
-
-                        <div className="iteamsrow-gray">
-                            <span>Items</span>
-                            <span>{ready}/{kot.items.length}</span>
-                        </div>
-                        {kot.items.map(item => {
-                            return (
-                                <div 
-                                    className={item.status === "served" ? "iteamsrow checkedrow" : "iteamsrow"} 
-                                    key={item.id}
-                                >
-                                    <div className="w-15">
-                                        <i className={item.status === "served" ? 'far fa-check-square' : 'far fa-square'} 
-                                            onClick={() => handleCheckMark(item, kot)} 
-                                        />
-                                    </div>
-                                    <div className="w-70">
-                                        <h5>{item.name}</h5>
-                                    </div>
-                                    <div className="w-15 text-right">
-                                        x<span className="bigfont">{item.quantity}</span>
-                                        {item.instructions && item.instructions !== '' ? <img src="/images/icon/info-icon-new.png" /> : null}
-                                    </div>
-                                </div>
-                            )
-                        })}
-                        <div className="iteamsrow text-center">
-                            <button 
-                                type="button" 
-                                onClick = {() => {
-                                    handleServed(kot)
-                                }} 
-                                className="btn served_kot"
-                            >
-                                Served
-                            </button>
-                        </div>
-                    </div>
+    let orders = table.data().orders;
+    orders.forEach((item) => {
+      if (item.orderPageId === it.orderPageId) {
+        if (flag) item.status = "cooking";
+        else item.status = "served";
+      }
+    });
+    ref.update({
+      orders,
+    });
+  };
+  return (
+    <div className="list-kot">
+      {kotItems &&
+        kotItems.map((kot) => {
+          let served = false;
+          let ready = 0;
+          kot.items.forEach((item) => {
+            if (item.status === "served") {
+              ready++;
+            }
+          });
+          if (ready === kot.items.length) {
+            served = true;
+          }
+          return (
+            <div className="box-kot" key={kot.id}>
+              <div className={served ? "kot-card selected" : "kot-card"}>
+                <div className="headrow">
+                  <h1>
+                    {kot.type || "DineIn"}{" "}
+                    <i
+                      className="fa fa-circle dinein_color"
+                      aria-hidden="true"
+                    ></i>
+                    <span>
+                      <i className="fas fa-ellipsis-v"></i>
+                    </span>
+                  </h1>
                 </div>
-            ) 
-        })}
-        </div>
-    )
-}
 
-export default CardView
+                <div className="main-head">
+                  <span>Table: {kot.tableName}</span>
+                  <span>
+                    <Timer
+                      initialTime={Date.now() - kot.createdOn}
+                      onStop={() => handleTimerStop(kot)}
+                    >
+                      {({ start, stop }) => {
+                        if (served) stop();
+                        return (
+                          <React.Fragment>
+                            <Timer.Hours />:
+                            <Timer.Minutes />:
+                            <Timer.Seconds />
+                          </React.Fragment>
+                        );
+                      }}
+                    </Timer>
+                  </span>
+                </div>
+
+                <div className="waiterrow">
+                  {kot.orderId || `0931280AASD90`}
+                </div>
+
+                <div className="iteamsrow-gray">
+                  <span>Items</span>
+                  <span>
+                    {ready}/{kot.items.length}
+                  </span>
+                </div>
+                {kot.items.map((item) => {
+                  return (
+                    <div
+                      className={
+                        item.status === "served"
+                          ? "iteamsrow checkedrow"
+                          : "iteamsrow"
+                      }
+                      key={item.id}
+                    >
+                      <div className="w-15">
+                        <i
+                          className={
+                            item.status === "served"
+                              ? "far fa-check-square"
+                              : "far fa-square"
+                          }
+                          onClick={() => handleCheckMark(item, kot)}
+                        />
+                      </div>
+                      <div className="w-70">
+                        <h5>{item.name}</h5>
+                      </div>
+                      <div className="w-15 text-right">
+                        x<span className="bigfont">{item.quantity}</span>
+                        {item.instructions && item.instructions !== "" ? (
+                          <img src="/images/icon/info-icon-new.png" />
+                        ) : null}
+                      </div>
+                    </div>
+                  );
+                })}
+                <div className="iteamsrow text-center">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      handleServed(kot);
+                    }}
+                    className="btn served_kot"
+                  >
+                    Served
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default CardView;
--- src/updateddesigns/LiveCart/orderPage.js
+++ src/updateddesigns/LiveCart/orderPage.js
@@ -1,34 +1,34 @@
-import {db} from '../../config'
+import { db } from "../../config";
 import React, { useContext, useEffect, useState } from "react";
 import { dispatchContext, tableContext } from "./contexts";
-import OrderItem from './orderItem'
+import OrderItem from "./orderItem";
 
 const Orders = () => {
-  const [orderList, setOrderList] = useState()
-  const [total, setTotal] = useState(0)
-  const [discount, setDiscount] = useState(0)
-  const [tax, setTax] = useState(0)
+  const [orderList, setOrderList] = useState();
+  const [total, setTotal] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [tax, setTax] = useState(0);
   const [kotNum, setKotNum] = useState(0);
-  const [id, setId] = useState()
-  const [cartId, setCartId] = useState()
+  const [id, setId] = useState();
+  const [cartId, setCartId] = useState();
 
   const dbRef = useContext(tableContext);
-  const dispatch = useContext(dispatchContext)
+  const dispatch = useContext(dispatchContext);
 
   const handleKOTCart = async () => {
-    const businessId = sessionStorage.getItem("businessId")
-    let table = await dbRef.get()
-    let arr = []
-    var order = table.data().orders
-    let kotItems = []
-    let kotId = Math.floor(Math.random()*1000000000)
-    for(var i = 0; i < order.length; i++){
-      let it = order[i]
-      if(it.status === "NotKot") {
-        it.kotId = kotId
-        arr.push(it)
-        kotItems.push(it)
-        it.status = "cooking"
+    const businessId = sessionStorage.getItem("businessId");
+    let table = await dbRef.get();
+    let arr = [];
+    var order = table.data().orders;
+    let kotItems = [];
+    let kotId = Math.floor(Math.random() * 1000000000);
+    for (var i = 0; i < order.length; i++) {
+      let it = order[i];
+      if (it.status === "NotKot") {
+        it.kotId = kotId;
+        arr.push(it);
+        kotItems.push(it);
+        it.status = "cooking";
       }
     }
     const kot = {
@@ -39,90 +39,91 @@ const Orders = () => {
       employee: table.data().currentEmployee,
       orderId: table.data().orderId,
       tableId: table.id,
-      type: "DineIn"
-    } 
+      type: "DineIn",
+    };
 
-    await db.collection('kotItems').doc(kotId.toString()).set(kot)
+    await db.collection("kotItems").doc(kotId.toString()).set(kot);
     dispatch({
       type: "KOTModalShow",
-      items: arr 
-    })
+      items: arr,
+    });
     await dbRef.update({
-      orders: order
-    })
+      orders: order,
+    });
   };
 
-
   useEffect(() => {
-    let unsubscribe
+    let unsubscribe;
     const getOrders = async () => {
       if (dbRef) {
-        const table = await dbRef.get()
-        setOrderList(table.data().orders)
-        setId(table.data().orderId)
-        setCartId(table.data().liveCartId)
-        unsubscribe = dbRef.onSnapshot(table => {
-         setOrderList(table.data().orders)
-          setId(table.data().orderId)
-          setCartId(table.data().liveCartId)
-        })
+        const table = await dbRef.get();
+        setOrderList(table.data().orders);
+        setId(table.data().orderId);
+        setCartId(table.data().liveCartId);
+        unsubscribe = dbRef.onSnapshot((table) => {
+          setOrderList(table.data().orders);
+          setId(table.data().orderId);
+          setCartId(table.data().liveCartId);
+        });
       }
-    }
-    getOrders()
-    return unsubscribe
-  }, [dbRef])
+    };
+    getOrders();
+    return unsubscribe;
+  }, [dbRef]);
   useEffect(() => {
-    if(orderList){
-      let total = 0, discount = 0, tax = 0, kot = 0
-      orderList.forEach(item => {
-        if (item.status !== 'NotKot') {
-          kot++
+    if (orderList) {
+      let total = 0,
+        discount = 0,
+        tax = 0,
+        kot = 0;
+      orderList.forEach((item) => {
+        if (item.status !== "NotKot") {
+          kot++;
         }
-        total += item.price * item.quantity
-        discount += item.price * item.discount / 100 * item.quantity
-        tax += item.tax * item.price / 100 * item.quantity
-      })
-      setKotNum(kot)
-      setTotal(parseFloat(total).toFixed(2))
-      setDiscount(parseFloat(discount).toFixed(2))
-      setTax(parseFloat(tax).toFixed(2))
+        total += item.price * item.quantity;
+        discount += ((item.price * item.discount) / 100) * item.quantity;
+        tax += ((item.tax * item.price) / 100) * item.quantity;
+      });
+      setKotNum(kot);
+      setTotal(parseFloat(total).toFixed(2));
+      setDiscount(parseFloat(discount).toFixed(2));
+      setTax(parseFloat(tax).toFixed(2));
     }
-  }, [orderList])
-
+  }, [orderList]);
 
   const check = () => {
-    let flag = true
+    let flag = true;
     orderList.forEach((item) => {
-      if (item.status === "NotKot") flag = false
-    })
-    return flag
-  }
+      if (item.status === "NotKot") flag = false;
+    });
+    return flag;
+  };
 
   const handleBillThis = async () => {
-    if(!check()){
-      alert("All Items must have Kot")
+    if (!check()) {
+      alert("All Items must have Kot");
     } else {
-      let table = await dbRef.get()    
-      let billItem = table.data().bill
-      if(!billItem) billItem = []
-      if(billItem.length === 0) {
-        var id = Math.floor(Math.random() * 100000000)
+      let table = await dbRef.get();
+      let billItem = table.data().bill;
+      if (!billItem) billItem = [];
+      if (billItem.length === 0) {
+        var id = Math.floor(Math.random() * 100000000);
         dbRef.update({
-          billId: id
-        })
+          billId: id,
+        });
       }
-      let orders = table.data().orders
-      billItem.push(...orders)
+      let orders = table.data().orders;
+      billItem.push(...orders);
       dbRef.update({
         orders: [],
-        bill: billItem
-      })
+        bill: billItem,
+      });
       dispatch({
         type: "setBillPage",
-        select: 3 
-      })
+        select: 3,
+      });
     }
-  }
+  };
 
   return (
     <div className="order_id_cart_box col-md-12 m-t-20">
@@ -143,12 +144,12 @@ const Orders = () => {
           {orderList &&
             orderList.map((item, index) => {
               return (
-                <OrderItem 
-                 item={item} 
-                 index={index}
-                 dbRef={dbRef}
-                 key={index}
-                 />
+                <OrderItem
+                  item={item}
+                  index={index}
+                  dbRef={dbRef}
+                  key={index}
+                />
               );
             })}
         </div>
@@ -171,13 +172,18 @@ const Orders = () => {
             </p>
             <p>
               <span className="left discount">Discount (free delivery)</span>{" "}
-              <span className="right discount">₹ {parseFloat(discount).toFixed(2)}</span>
+              <span className="right discount">
+                ₹ {parseFloat(discount).toFixed(2)}
+              </span>
             </p>
             <p className="m-t-15">
               <span className="left grandtotal_font">Grand Total</span>{" "}
               <span className="right grand_font">
                 {" "}
-                ₹{parseFloat(total - parseFloat(discount) + parseFloat(tax)).toFixed(2)}
+                ₹
+                {parseFloat(
+                  total - parseFloat(discount) + parseFloat(tax)
+                ).toFixed(2)}
               </span>
             </p>
           </div>
